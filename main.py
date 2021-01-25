0996dbec76251421f0adc90dca3b1aecfacad790
Ryan Schulte <ryan.j.schulte@gmail.com>
2021-01-24 18:06:28 -0700

    walmart

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def ConsoleCheck(url, merchant_name):
+def AmazonConsoleCheck(url, merchant_name, executable_path):
 
     options = Options()
     options.add_argument('--headless')
     options.add_argument('ignore-certificate-errors')
     options.add_argument('--no-sandbox')
 
-    # try:
-    #     driver = webdriver.Chrome(executable_path='C:\Program Files\ChromeDriver\chromedriver.exe', options=options)
-    # except Exception as e:
-    #     print("driver not available")
-    #     print(f"Error: {str(e)}")
     try:
-        driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+        driver = webdriver.Chrome(executable_path=execurtable_path, options=options)
     except Exception as e:
         print("driver not available")
         print(f"Error: {str(e)}")
@@ -39,6 +34,21 @@ def ConsoleCheck(url, merchant_name):
         return
     return
 
+def WalmartConsoleCheck(url, merchant_name, executable_path):
+        try:
+        driver = webdriver.Chrome(executable_path=executable_path, options=options)
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, 'some link text')))
+    except Exception as e:
+        print("driver not available")
+        print(f"Error: {str(e)}")
+        return
+
+
 if __name__ == '__main__':
+    chrome_executable_path = "'/usr/local/bin/chromedriver'"
+
     amazon_url = "https://www.amazon.com/Xbox-X/dp/B08H75RTZ8/ref=sr_1_16?crid=1JXVRXKXUG4Y9&dchild=1&keywords=xbox+series+x&qid=1611521867&sprefix=xbox%2Caps%2C221&sr=8-16"
-    ConsoleCheck(url=amazon_url, merchant_name="amazon")
+    AmazonConsoleCheck(url=amazon_url, merchant_name="amazon", executable_path=chrome_executable_path)
+
+    walmart_url = "https://www.walmart.com/ip/Xbox-Series-X/443574645"
+
